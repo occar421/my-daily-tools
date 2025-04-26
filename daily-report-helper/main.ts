b615edb43fb88598f8f39a6e60e36d86267a556d
MasuqaT <occar@hotmail.co.jp>
2025-04-27 08:07:59 +0900

    Simple convert

--- daily-report-helper/main.ts
+++ daily-report-helper/main.ts
@@ -1,12 +1,13 @@
-import { join, parse } from "jsr:@std/path";
+import { join, parse as parsePath } from "jsr:@std/path";
+import { parse as parseCsv } from "jsr:@std/csv";
 
-type Record = {
+type ReportRecord = {
   epoch: number;
   title: string;
   meta: string;
 };
 
-const records: Record[] = [];
+const records: ReportRecord[] = [];
 
 const baseDir = join(import.meta.dirname ?? ".", "data");
 for await (
@@ -14,11 +15,13 @@ for await (
 ) {
   if (entry.isFile) {
     const path = join(baseDir, entry.name);
-    const parsedPath = parse(path);
+    const parsedPath = parsePath(path);
 
     switch (parsedPath.ext) {
       case ".csv":
-        records.push(...csvToRecord(await Deno.readTextFile(path)));
+        records.push(
+          ...chromeHistoryCsvToRecord(await Deno.readTextFile(path)),
+        );
         break;
       default:
         continue;
@@ -28,8 +31,53 @@ for await (
   }
 }
 
-function csvToRecord(csv: string): Record[] {
-  // TODO
+/**
+ * CSVテキストからレコードを作成する関数
+ * @param text CSVテキスト
+ * @returns 抽出されたRecord型オブジェクトの配列
+ */
+function chromeHistoryCsvToRecord(text: string): ReportRecord[] {
+  const records: ReportRecord[] = [];
 
-  return [];
+  const csv: Record<"date" | "time" | "title" | "url", string>[] = parseCsv(
+    text,
+    {
+      skipFirstRow: true,
+      strip: true,
+    },
+  );
+
+  for (const row of csv) {
+    try {
+      // 日付と時間からエポック時間を算出
+      // us order
+      const [month, day, year] = row.date.split("/").map(Number);
+      const [hour, minute] = row.time.split(":").map(Number);
+
+      if (
+        isNaN(year) || isNaN(month) || isNaN(day) ||
+        isNaN(hour) || isNaN(minute)
+      ) {
+        console.error(`不正な日付形式: ${row.date} ${row.time}`);
+        continue;
+      }
+
+      // Javascriptの月は0から始まるため、月から1を引く
+      const dateObj = new Date(year, month - 1, day, hour, minute);
+      const epoch = dateObj.getTime();
+
+      // Recordオブジェクトを作成して配列に追加
+      records.push({
+        epoch,
+        title: row.title,
+        meta: row.url,
+      });
+    } catch (error) {
+      console.error(`日付変換エラー: ${row.date} ${row.time}`, error);
+    }
+  }
+
+  console.debug("Records: ", records);
+
+  return records;
 }
