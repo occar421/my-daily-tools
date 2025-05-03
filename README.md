b55843e3845180202018096b6fe814d052939a68
MasuqaT <occar@hotmail.co.jp>
2025-05-04 00:52:50 +0900

    add slack search result collector

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-daily-tools
+
+Credits
+
+- Downloading Slack results: https://qiita.com/xshoji/items/2973fb397261912ef830
---
+++ daily-report-helper/slack-export-bookmarklet.js
@@ -0,0 +1,301 @@
+(async () => {
+  const DEBUG_MODE = false;
+  const log = (value) => {
+    if (DEBUG_MODE === true) {
+      console.log(value);
+    }
+  };
+
+  /**
+   * Gather Slack messages in all page of search result.
+   * @param messagePack
+   */
+  const getMessageInner = async (messagePack) => {
+    log(">>> getMessageInner");
+    if (!messagePack.hasNextPage) {
+      log("messagePack.hasNextPage = " + messagePack.hasNextPage);
+      return;
+    }
+
+    /* Wait searched results and gather these messages */
+    await createPromiseWaitSearchResult();
+    do {
+      await wait(800);
+      await createPromiseGetMessages(messagePack);
+    } while (messagePack.messagePushed === true);
+    await createPromiseClickNextButton(messagePack);
+    await wait(600);
+    await createPromiseCheckOutOfPageLimit(messagePack);
+    await getMessageInner(messagePack);
+  };
+
+  const getMessage = async (messagePack) => {
+    log(">>> getMessage");
+    await getMessageInner(messagePack);
+  };
+
+  /**
+   * Wait display searched result.
+   */
+  const createPromiseWaitSearchResult = () => {
+    log(">>> createPromiseWaitSearchResult");
+    const selector = ".c-search_message__content";
+    const messageGroupSelector = ".c-message_group";
+    const messageTimestampSelector = ".c-timestamp";
+    const messageTimestampAttributeKey = "data-ts";
+
+    const observeFunc = () => {
+      let messageGroups = document.querySelectorAll(messageGroupSelector);
+      let completed = true;
+      messageGroups.forEach((messageGroup) => {
+        let timestampElm = messageGroup.querySelector(messageTimestampSelector);
+        if (!timestampElm) {
+          completed = false;
+          return;
+        }
+        let timestampAttributeValue = timestampElm.getAttribute(
+          messageTimestampAttributeKey,
+        );
+        if (!timestampAttributeValue) {
+          completed = false;
+        }
+      });
+
+      const el = document.querySelector(selector);
+      if (el && completed) {
+        return el;
+      }
+      return null;
+    };
+
+    return new Promise((resolve) => {
+      let observedElement = observeFunc();
+      if (observedElement !== null) {
+        resolve(observedElement);
+      }
+
+      new MutationObserver((mutationRecords, observer) => {
+        let observedElement = observeFunc();
+        if (observedElement !== null) {
+          resolve(observedElement);
+          /* Once we have resolved we don't need the observer anymore */
+          observer.disconnect();
+        }
+      })
+        .observe(document.documentElement, {
+          childList: true,
+          subtree: true,
+        });
+    });
+  };
+
+  /**
+   * Get message
+   */
+  const createPromiseGetMessages = async (messagePack) => {
+    log(">>> createPromiseGetMessages");
+    const messageGroupSelector = ".c-message_group";
+    const messageContentSelector = ".c-search_message__content";
+    const messageTimestampSelector = ".c-timestamp";
+    const messageTimestampAttributeKey = "data-ts";
+    const channelNameSelector = ".c-message_group__header";
+    const messageSenderSelector = ".c-message__sender_button";
+    const timestampLabelSelector = ".c-timestamp__label";
+
+    return new Promise((resolve) => {
+      messagePack.messagePushed = false;
+      let messageGroups = document.querySelectorAll(messageGroupSelector);
+      log(
+        "createPromiseGetMessages | Promise | messageGroups.length = " +
+          messageGroups.length,
+      );
+
+      messageGroups.forEach((messageGroup) => {
+        const datetime = timestampToTime(
+          messageGroup.querySelector(messageTimestampSelector).getAttribute(
+            messageTimestampAttributeKey,
+          ).split(".")[0],
+        );
+        /* qiita_twitter_bot */
+        const channelName =
+          messageGroup.querySelector(channelNameSelector).textContent;
+        /* twitter */
+        const messageSender =
+          messageGroup.querySelector(messageSenderSelector).textContent;
+        /* 8:00 PM */
+        const timestampLabel =
+          messageGroup.querySelector(timestampLabelSelector).textContent;
+        /* twitterAPP 8:00 PM slack message here ...  */
+        const message =
+          messageGroup.querySelector(messageContentSelector).textContent;
+        const removeMessageSender = new RegExp(
+          "^" + escapeRegExp(messageSender),
+        );
+        const removeTimestampLabel = new RegExp("^.*?" + timestampLabel);
+        /* APP 8:00 PM slack message here ...  */
+        const trimmedMessage = message.replace(removeMessageSender, "").replace(
+          removeTimestampLabel,
+          "",
+        );
+        /* 2020/12/19 20:00:20 <tab> qiita_twitter_bot <tab> twitter <tab> slack message here ...  */
+        const timeAndMessage = datetime + "\t" + channelName + "\t" +
+          messageSender + "\t" + trimmedMessage;
+        log(
+          "createPromiseGetMessages | Promise | messageGroups.forEach | " +
+            [datetime, channelName, messageSender, timestampLabel, message]
+              .join(", "),
+        );
+        log(
+          "createPromiseGetMessages | Promise | messageGroups.forEach | " +
+            timeAndMessage,
+        );
+        if (messagePack.messageSet.has(timeAndMessage)) {
+          log(
+            "createPromiseGetMessages | Promise | messagePack.messageSet.has(timeAndMessage) === true | " +
+              timeAndMessage,
+          );
+          return;
+        }
+        messagePack.messages.push(timeAndMessage);
+        messagePack.messagePushed = true;
+        messagePack.messageSet.add(timeAndMessage);
+        messageGroup.scrollIntoView();
+      });
+      resolve(messagePack);
+    });
+  };
+
+  /**
+   * Click next page link
+   */
+  const createPromiseClickNextButton = (messagePack) => {
+    log(">>> createPromiseClickNextButton");
+
+    const arrowBtnElements = document.querySelectorAll(
+      ".c-pagination__arrow_btn",
+    );
+    let nextArrowBtnElement = null;
+    messagePack.hasNextPage = false;
+    if (arrowBtnElements.length === 0) {
+      /* Return dummy promise */
+      return Promise.resolve(messagePack);
+    }
+    arrowBtnElements.forEach((e) => {
+      if (["Next page", "次のページ"].includes(e.getAttribute("aria-label"))) {
+        nextArrowBtnElement = e;
+      }
+    });
+    if (!nextArrowBtnElement) {
+      log("createPromiseClickNextButton | Next page button not found.");
+      return Promise.resolve(messagePack);
+    }
+    messagePack.hasNextPage =
+      nextArrowBtnElement.attributes["aria-disabled"].value === "false";
+    if (!messagePack.hasNextPage) {
+      log(
+        "createPromiseClickNextButton | messagePack.hasNextPage = " +
+          messagePack.hasNextPage,
+      );
+      /* Return dummy promise */
+      return Promise.resolve(messagePack);
+    }
+    return new Promise((resolve) => {
+      log("createPromiseClickNextButton | Promise | click()");
+      nextArrowBtnElement.click();
+      resolve(messagePack);
+    });
+  };
+
+  /**
+   * Check if the next page is out of the page limit
+   */
+  const createPromiseCheckOutOfPageLimit = (messagePack) => {
+    log(">>> createPromiseCheckOutOfPageLimit");
+    const selector = ".c-search_message__content";
+    let el = document.querySelector(selector);
+    if (el === null) {
+      messagePack.hasNextPage = false;
+    }
+    return Promise.resolve(messagePack);
+  };
+
+  /**
+   * Wait specified millisecond
+   */
+  const wait = (millisecond) => {
+    return new Promise((resolve) => setTimeout(resolve, millisecond));
+  };
+
+  /**
+   * timestamp to datetame
+   * @param timestamp
+   * @returns {string}
+   */
+  const timestampToTime = (timestamp) => {
+    const d = new Date(timestamp * Math.pow(10, 13 - timestamp.length));
+    const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const yyyy = d.getFullYear();
+    const mm = ("0" + (d.getMonth() + 1)).slice(-2);
+    const dd = ("0" + d.getDate()).slice(-2);
+    const hh = ("0" + d.getHours()).slice(-2);
+    const mi = ("0" + d.getMinutes()).slice(-2);
+    const ss = ("0" + d.getSeconds()).slice(-2);
+    const week = weekday[d.getDay()];
+    return `${yyyy}-${mm}-${dd} ${week} ${hh}:${mi}:${ss}`;
+  };
+
+  /**
+   * Escape regex meta characters
+   * > Escape string for use in Javascript regex - Stack Overflow
+   * > https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+   * @param stringValue
+   * @returns {*}
+   */
+  const escapeRegExp = (stringValue) => {
+    /* $& means the whole matched string */
+    return stringValue.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  };
+
+  /**
+   * Download list as a file
+   * @param messagePack
+   * @returns {boolean}
+   */
+  const download = (messagePack) => {
+    log(">>> download");
+    const massageAll = messagePack.messages.join("\n");
+    log(
+      "download | messagePack.messages.length " + messagePack.messages.length,
+    );
+    log("download | massageAll.length " + massageAll.length);
+
+    const link = document.createElement("a");
+    link.style.display = "none";
+    link.href = window.URL.createObjectURL(
+      new Blob([massageAll], { type: "text/plain" }),
+    );
+    link.download = "slack_messages.tsv";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    return true;
+  };
+
+  const exportMessage = async () => {
+    log(">>> exportMessage");
+    const messagePack = {
+      messages: [],
+      messageSet: new Set(),
+      messagePushed: false,
+      hasNextPage: true, /* To handle a first loop */
+    };
+
+    /* Gather messages in all pages */
+    await getMessage(messagePack);
+
+    download(messagePack);
+  };
+
+  /* Run */
+  await exportMessage();
+})();
